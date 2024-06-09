1aa9582b7f4c645006b934f0f466ddcffef5a0ec
LilianLaporte <lilap74@yahoo.fr>
2024-06-09 11:35:08 +0200

    update general informations

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Student in Robotics, Systems and Contol",
+          "Intern at MESH AG",
         ],
         autoStart: true,
         loop: true,
