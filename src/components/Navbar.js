0401c9f90b15053496b8fba3a04ed4df1c6f77d1
LilianLaporte <lilap74@yahoo.fr>
2024-07-01 08:13:53 +0200

    add projects bakc

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -59,6 +59,18 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/project"
+                onClick={() => updateExpanded(false)}
+              >
+                <AiOutlineFundProjectionScreen
+                  style={{ marginBottom: "2px" }}
+                />{" "}
+                Projects
+              </Nav.Link>
+            </Nav.Item>
 
             <Nav.Item>
               <Nav.Link
