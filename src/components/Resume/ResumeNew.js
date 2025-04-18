756ff98f1dfe5fd0cde6b52f26b10bfa64371816
LilianLaporte <lilap74@yahoo.fr>
2025-04-18 11:12:19 +0200

    fix resume errors

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_Lilian.pdf";
+import pdf from "../../Assets/CV_Lilian.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -31,7 +32,7 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} renderAnnotationLayer={false} renderTextLayer={false}/>
           </Document>
         </Row>
 
